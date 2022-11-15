c4c7f71abb85de6b57cdd95013469df896d0c8c7
Prameesh <prameepramee0@gmail.com>
2022-11-15 07:16:24 +0530

    queue

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+// Struct for queue
+type queue struct {
+	values []int
+//values slice inside the struct
+}
+func (q *queue) enqueue(i int) {
+	
+//enqueue is a method for append the values to slice.
+	
+q.values = append(q.values, i)
+}
+
+func main() {   //main function
+
+	myqueue := queue{}
+	myqueue.enqueue(1);myqueue.enqueue(2);myqueue.enqueue(3);
+	myqueue.enqueue(4);myqueue.enqueue(5);fmt.Println(myqueue)
+	myqueue.dequeue();fmt.Println(myqueue)
+	myqueue.dequeue();fmt.Println(myqueue)
+	myqueue.dequeue();fmt.Println(myqueue)
+
+}
+//method dequeue for remove elements from the slice 
+ func (q *queue) dequeue()int{
+	toremove:=q.values[0]
+	q.values=q.values[1:]
+	return toremove}
