0d10707376e461fbebcae0b8403d3c91ce36db97
Prameesh <prameepramee0@gmail.com>
2022-11-15 07:24:55 +0530

    space added

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
+
 import "fmt"
+
 // Struct for queue
+
 type queue struct {
+
 	values []int
+
 //values slice inside the struct
+
 }
+
 func (q *queue) enqueue(i int) {
 	
 //enqueue is a method for append the values to slice.
 	
 q.values = append(q.values, i)
+
 }
 
 func main() {   //main function
@@ -22,8 +30,13 @@ func main() {   //main function
 	myqueue.dequeue();fmt.Println(myqueue)
 
 }
+
 //method dequeue for remove elements from the slice 
- func (q *queue) dequeue()int{
+ 
+func (q *queue) dequeue()int{
+	
 	toremove:=q.values[0]
 	q.values=q.values[1:]
-	return toremove}
+	return toremove
+
+}
